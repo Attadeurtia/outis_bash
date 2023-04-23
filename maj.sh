07d2309e8f1a4957c3623a9c76f24b4efd811fc6
attadeurtia <attadeurtia@hotmail.com>
2023-04-23 17:58:50 +0200

    suppresion des ===> qui rajoute du delaye et vérification que flatpack est installé avant de le mettre à jour

--- maj.sh
+++ maj.sh
@@ -3,74 +3,54 @@
 # Name:             maj.sh
 # Author:           Geoffrey Posé
 # Creation:         03.11.2021
+# Modified:         23.04.2023
 # Description:      mets à jours les apt et les flatpacks et nettoie les repertoires.
 # Documentation:    https://github.com/Attadeurtia/outis_bash
 ######################################################
-                                                                    
-# test si on est root                                                           
-test=`whoami`
+
+# test si on est root
+test=$(whoami)
 if [ $test != "root" ]; then
-  echo "a lancer en root ou sous sudo"
+  echo "sudo ou root requis"
   exit
 fi
 
-while [ $i -le 25 ]
-do
-printf "="
-sleep 1
-let i=$i+1
-done
-echo -e ">]"
-
-#Update apt 
-echo "apt update"                                                            
+#Update apt
+echo "--------------------------------"
+echo "apt update"
 apt update
-printf "["
-i=0
-while [ $i -le 5 ]
-do
-printf "="
-sleep 1
-let i=$i+1
-done
-echo -e ">]"
 echo -e "Update System  [ \033[32m OK \033[0m]"
 
 #Upgrade apt
 echo "--------------------------------"
-echo "apt upgrade"                                                                     
+echo "apt upgrade"
 apt -y upgrade
 apt -y dist-upgrade
-printf "["
-i=0
-while [ $i -le 5 ]
-do
-printf "="
-sleep 1
-let i=$i+1
-done
-echo -e ">]"
 echo -e "Upgrade System [ \033[32m OK \033[0m]"
-apt -y autoremove --purge && apt autoclean
 
-#Upgrade flatpack                                                                       
+#Clean apt
 echo "--------------------------------"
-echo "flatpack"
-flatpak update -y
+echo "apt autoremove --purge && apt autoclean"
+apt -y autoremove --purge
+apt autoclean
+echo -e "Clean System [ \033[32m OK \033[0m]"
 
-printf "["
-i=0
-while [ $i -le 5 ]
-do
-printf "="
-sleep 1
-let i=$i+1
-done
-echo -e ">]"
-echo -e "Udpate System [ \033[32m OK \033[0m]"
-
+#vérifier si flatpack est installé
 echo "--------------------------------"
-echo "flatpak uninstall --unused"
-flatpak uninstall --unused
+flatpak --version
+if [ $? -eq 0 ]; then
 
-echo -e "clean System [ \033[32m OK \033[0m]"
+  #Upgrade flatpack
+  echo "--------------------------------"
+  echo "flatpack update"
+  flatpak update -y
+  echo -e "Udpate System [ \033[32m OK \033[0m]"
+
+  echo "--------------------------------"
+  echo "flatpak uninstall --unused"
+  flatpak uninstall --unused
+  echo -e "clean System [ \033[32m OK \033[0m]"
+
+else
+  echo "flatpack n'est pas installé"
+fi
